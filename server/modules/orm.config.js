9a3e269106b0c56edf327c06b6a3ee6971383db9
Blaine Booher <blaine@emergingprairie.com>
2020-12-16 16:48:33 -0600

    updating orm to be a bit more verbose

--- server/modules/orm.config.js
+++ server/modules/orm.config.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 
 // Configure Sequelize instance from DATABASE_URL env variable
 // Default to an in-memory database if DATABASE_URL is not found
+if (!process.env.DATABASE_URL) console.log('DATABASE_URL not found, defaulting to sqlite://:memory in-memory database');
 let sequelize = new Sequelize(process.env.DATABASE_URL || 'sqlite://:memory');
 
 // Optional: Test the connection
