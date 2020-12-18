c109c24d34aa956864df8f0845fe763e6da99103
Jaryd Krishnan <jarydkrishnan@gmail.com>
2020-12-17 21:43:38 -0600

    Add some routing

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -3,27 +3,66 @@ const Task = require('../models/task.model');
 const router = express.Router();
 
 // GET /tasks/
-router.get('/', (req, res) => {
-   // TODO: Get a list of all tasks and send to the browser with 200 OK
-   res.sendStatus(500);
+router.get('/', async (req, res) => {
+  try {
+    const tasks = await Task.findAll();
+    res.send(tasks);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 // POST /tasks/
-router.post('/', (req, res) => {
-   // TODO: Build a new task model, save it, and send back 201 CREATED
-   res.sendStatus(500);
+router.post('/', async (req, res) => {
+  try {
+    const { description } = req.body;
+    const task = await Task.create({
+      description,
+      done: false,
+    });
+    res.status(201).send(task);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 // PUT /tasks/:id
-router.put('/:id', (req, res) => {
-   // TODO: Update a task status, save it, and send back 204 NO CONTENT
-   res.sendStatus(500);
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findOne({ where: { id } });
+    if (!task) {
+      res.sendStatus(404);
+      return;
+    }
+    const { description, done } = req.body;
+    task.description = description;
+    task.done = done;
+    await task.save();
+    res.send(task);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 // DELETE /tasks/:id
-router.delete('/:id', (req, res) => {
-   // TODO: Delete a task and send back 204 NO CONTENT
-   res.sendStatus(500);
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findOne({ where: { id } });
+    if (!task) {
+      res.sendStatus(404);
+      return;
+    }
+    await task.destroy();
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
